7062ad5d54cea7aa2af24d3ffa91041bde813192
Jeremy <jeremy.valentin@gmail.com>
2016-10-15 14:51:42 +0200

    Default MSBUILD Path in order to build it throw Jenkins command line

--- build.bat
+++ build.bat
@@ -1,15 +1,17 @@
 echo off
 
-set TARGET=%1
-set CONFIGURATION=%2
-set NET_VERSION=%3
-set TOOLS_VERSION=%4
+set MSBUILD_PATH=%1
+set TARGET=%2
+set CONFIGURATION=%3
+set NET_VERSION=%4
+set TOOLS_VERSION=%5
 
-if "%1" == "" set TARGET=Rebuild
-if "%2" == "" set CONFIGURATION=Release
-if "%3" == "" set NET_VERSION=v4.5.2
-if "%4" == "" set TOOLS_VERSION=14.0
+if "%1" == "" set MSBUILD_PATH=C:\Program Files (x86)\MSBuild\14.0\Bin\
+if "%2" == "" set TARGET=Rebuild
+if "%3" == "" set CONFIGURATION=Release
+if "%4" == "" set NET_VERSION=v4.5.2
+if "%5" == "" set TOOLS_VERSION=14.0
 
-set BUILD_CMD=MSBuild.exe QuickFIXn.sln /t:%TARGET% /p:Configuration=%CONFIGURATION%;TargetFrameworkVersion=%NET_VERSION% /tv:%TOOLS_VERSION%
+set BUILD_CMD="%MSBUILD_PATH%MSBuild.exe" QuickFIXn.sln /t:%TARGET% /p:Configuration=%CONFIGURATION%;TargetFrameworkVersion=%NET_VERSION% /tv:%TOOLS_VERSION%
 echo Build command: %BUILD_CMD%
 %BUILD_CMD%
