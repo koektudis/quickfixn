385df3e97a6f4d6d8c32dff02f69b37db70bf27c
Jeremy <jeremy.valentin@gmail.com>
2016-10-16 15:22:16 +0200

    Use Nunit2 format for result

--- unit_test.bat
+++ unit_test.bat
@@ -5,7 +5,7 @@ set RESULT=0
 pushd UnitTests\bin\Release\
     if exist TestResult.xml del TestResult.xml
     if exist UnitTests.html del UnitTests.html
-    "C:\Program Files (x86)\NUnit-3.5.0\bin\net-4.5\nunitlite-runner.exe" UnitTests.dll
+    "C:\Program Files (x86)\NUnit-3.5.0\bin\net-4.5\nunitlite-runner.exe" UnitTests.dll --result=TestResult.xml;format=nunit2
     if ERRORLEVEL 1 set RESULT=1
 popd
 
